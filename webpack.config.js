48f1700ef4923df9d647e0675a3529a5d73bc4dc
Chris Rinaldi <cgrinaldi@gmail.com>
2015-12-15 17:07:55 -0500

    Initialize project - Webpack setup, file structure, etc.

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/js/index.jsx',
+    './src/index.html',
+    './src/styles.scss'
+  ],
+
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+
+  devtool: 'source-map',
+
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        include: path.join(__dirname, 'src', 'js'),
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015','react','stage-0']
+        }
+      },
+      {
+        test: /\.html$/,
+        loader: 'file?name=[name].[ext]'
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style!css!sass'
+      }
+    ]
+  },
+
+  debug: true,
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
