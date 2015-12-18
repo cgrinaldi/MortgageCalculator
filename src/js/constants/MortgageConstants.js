6f54b9252de09b64365e32eee23ef27ae153910d
Chris Rinaldi <cgrinaldi@gmail.com>
2015-12-18 15:48:46 -0500

    Create functions and tests for calculating monthly PITI and downpayment
    Given a product (isRex, insurance prem, and downpayment amount),
    home price, and various rates, calculate the monthly PITI
    (principal, interest, taxes, and insurance).

---
+++ src/js/constants/MortgageConstants.js
@@ -0,0 +1,24 @@
+export const MI_PREM_035 = 0.0085;
+export const MI_PREM_10 = 0.004;
+
+export const PRODUCT_035 = {
+  downpaymentFraction: 0.035,
+  MIPrem: MI_PREM_035
+};
+
+export const PRODUCT_10 = {
+  downpaymentFraction: 0.1,
+  MIPrem: MI_PREM_10
+};
+
+export const PRODUCT_20 = {
+  downpaymentFraction: 0.2,
+  MIPrem: 0
+};
+
+
+export const PRODUCT_REX = {
+  isREX: true,
+  downpaymentFraction: 0.1,
+  MIPrem: 0
+};
