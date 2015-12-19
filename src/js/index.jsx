909b61c54b552510768b4c316501eba9ac59f7f6
Chris Rinaldi <cgrinaldi@gmail.com>
2015-12-19 15:10:20 -0500

    Configure store

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route} from 'react-router';
+import configureStore from './store/configureStore';
 
 import App from './containers/App';
 import Intro from './containers/Intro';
@@ -8,6 +9,10 @@ import GettingStarted from './containers/GettingStarted';
 import ApprovalTool from './containers/ApprovalTool';
 import CalcPITI from './containers/CalcPITI';
 import CalcMaxPurchase from './containers/CalcMaxPurchase';
+
+const store = configureStore();
+// Quick test to make sure the store is setup correctly
+store.dispatch({type: 'SET_HOMEPRICE', val: 350000});
 
 ReactDOM.render(
   <Router>
